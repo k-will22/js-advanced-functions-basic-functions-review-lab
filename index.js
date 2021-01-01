68bc1c5d8b1ea4d2753d9bab614e577513b9fd9c
k-will22 <thestreetkw@hotmail.com>
2021-01-01 18:22:22 -0500

    commit tree 32cf66c522b02f126f8ec8f65fab105bf68b861d

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(param="special") {
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: function(a, b) {
+        return a + b
+    },
+
+    subtract: function(a, b) {
+        return a - b
+    },
+
+    multiply: function(a, b) {
+        return a * b
+    },
+
+    divide: function(a, b) {
+        return a / b
+    }
+}
+
+function actionApplyer(string, array) {
+    let a = string 
+
+    for(let i = 0; i < array.length; i++){
+        a = array[i](a)
+    }
+    return a 
+}
